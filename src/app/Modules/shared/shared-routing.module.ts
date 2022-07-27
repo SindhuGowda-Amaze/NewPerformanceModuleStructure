024c663ec1a0b64d6785a85ffae919c94a10706d
SindhuGowda-Amaze <you@example.com>
2022-07-27 17:50:08 +0530

    ddd

--- src/app/Modules/shared/shared-routing.module.ts
+++ src/app/Modules/shared/shared-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SharedComponent } from './shared.component';
 
-const routes: Routes = [{ path: '', component: SharedComponent }];
+const routes: Routes = [{ path: '', component: SharedComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
